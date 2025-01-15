074e44cab9b24b3909d4f70ae47548c7c357f644
Alicia de Wet <alicia.dewet@gmail.com>
2025-01-15 17:31:23 +0000

    :construction: add get single recipe api call

--- src/stores/recipe.ts
+++ src/stores/recipe.ts
@@ -18,11 +18,13 @@ interface Recipe {
   rating: number;
   reviewCount: number;
   mealType: string[];
+  favourite: boolean;
 }
 
 export const useRecipeStore = defineStore('recipe', () => {
   // refs - state
   const recipes = ref<Recipe[]>([]);
+  const recipe = ref<Recipe | null>(null);
 
   // computed - getters
 
@@ -31,16 +33,28 @@ export const useRecipeStore = defineStore('recipe', () => {
     const response = await fetch('https://dummyjson.com/recipes');
 
     if (response) {
-      const recipeResponse = await response.json();
+      const recipesResponse = await response.json();
 
-      recipes.value = recipeResponse.recipes;
+      recipes.value = recipesResponse.recipes;
+    } else {
+      // handle no response
+    }
+  };
+
+  const getSingleRecipe = async (id: string): Promise<void> => {
+    const response = await fetch(`https://dummyjson.com/recipes/${id}`);
+
+    if (response) {
+      recipe.value = await response.json();
     } else {
       // handle no response
     }
   };
 
   return {
+    recipe,
     recipes,
     getRecipes,
+    getSingleRecipe,
   };
 });
