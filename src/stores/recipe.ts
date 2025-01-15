e29ee521a4e65857a426422566ebf141319b064f
Alicia de Wet <alicia.dewet@gmail.com>
2025-01-15 16:25:12 +0000

    :sparkles: get all recipes to populate landing page

--- src/stores/recipe.ts
+++ src/stores/recipe.ts
@@ -1,5 +1,46 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+interface Recipe {
+  id: string;
+  name: string;
+  ingredients: string[];
+  instructions: string[];
+  prepTimeMinutes: number;
+  cookTimeMinutes: number;
+  servings: number;
+  difficulty: string;
+  cuisine: string;
+  caloriesPerServing: number;
+  tags: string[];
+  userId: number;
+  image: string;
+  rating: number;
+  reviewCount: number;
+  mealType: string[];
+}
 
 export const useRecipeStore = defineStore('recipe', () => {
-  // store
+  // refs - state
+  const recipes = ref<Recipe[]>([]);
+
+  // computed - getters
+
+  // functions - actions
+  const getRecipes = async (): Promise<void> => {
+    const response = await fetch('https://dummyjson.com/recipes');
+
+    if (response) {
+      const recipeResponse = await response.json();
+
+      recipes.value = recipeResponse.recipes;
+    } else {
+      // handle no response
+    }
+  };
+
+  return {
+    recipes,
+    getRecipes,
+  };
 });
