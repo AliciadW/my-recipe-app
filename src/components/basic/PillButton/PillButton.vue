928b93950763dec5c9b3d8236ec9b6aad0e1ba61
Alicia de Wet <alicia.dewet@gmail.com>
2025-01-15 17:28:51 +0000

    :sparkles: add pill button component

---
+++ src/components/basic/PillButton/PillButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps(['content', 'type']);
+</script>
+
+<template>
+  <div class="rounded-full text-xs w-fit bg-indigo-200 py-1 px-1.5 mr-1 mt-1">
+    <p>{{ content }}</p>
+  </div>
+</template>
