c131f1739aa4488d247be46be369ebb6b8954900
Alicia de Wet <alicia.dewet@gmail.com>
2025-01-16 15:29:29 +0000

    :label: move navigation item type

--- src/types/ComponentTypes.ts
+++ src/types/ComponentTypes.ts
@@ -1,4 +1,12 @@
+import type { FunctionalComponent, HTMLAttributes, VNodeProps } from 'vue';
+
 export interface PillButtonType {
   content?: string | number;
   type?: string;
+}
+
+export interface NavigationItem {
+  name: string;
+  routeName: string;
+  icon: FunctionalComponent<HTMLAttributes & VNodeProps>;
 }
